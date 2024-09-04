614b4560e48f1f69274877c370fb74bbd9cb2646
Hiago Laureano <hiagolaureano@hotmail.com>
2024-09-04 15:34:33 -0300

    create Footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <NavBar/>
-  <router-view/>
+  <router-view :API_URL="API_URL"/>
+  <Footer/>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue"
+import Footer from "./components/Footer.vue"
 export default{
   components: {
-    NavBar
+    NavBar,
+    Footer
+  },
+  data(){
+    return{
+      API_URL: "http://127.0.0.1:8000"
+    }
   }
 }
 </script>
@@ -17,26 +25,22 @@ export default{
   font-family: Arial, Helvetica, sans-serif;
   margin: 0;
 }
-
 .bg-primary-color{
   background: rgb(2, 83, 104);
 }
-
 .bg-secondary-color{
   background: whitesmoke;
 }
-
 .primary-color{
   color:whitesmoke
 }
 .secondary-color{
   color: rgb(2, 83, 104);
 }
-
 .btn-app-primary{
   background: rgb(2, 83, 104);
   border-radius: 10px;
-  padding: 8px;
+  padding: 10px;
   color: whitesmoke;
   font-weight: bold;
   border: none;
@@ -45,22 +49,36 @@ export default{
 .btn-app-secondary{
   background: whitesmoke;
   border-radius: 10px;
-  padding: 8px;
+  padding: 10px;
   color: rgb(2, 83, 104);
   font-weight: bold;
   border: none;
   box-shadow: 2px 2px 3px #222;
-
 }
-
 .input-app{
   border-radius: 10px;
   padding: 10px;
   border: none;
   background: whitesmoke;
+  border: 1px solid rgb(2, 83, 104);
+  box-shadow: 2px 2px 3px #222;
 }
 .input-app:focus{
   outline: none;
 }
-
+hr{
+  width: 90%;
+  height: 1px;
+  border-bottom: 1px solid #ccc;
+}
+.select-app{
+  border-radius: 10px 10px 0 0;
+  padding: 10px;
+  background: whitesmoke;
+  border: 2px solid rgb(2, 83, 104);
+  box-shadow: 2px 2px 3px #222;
+}
+.select-app:focus{
+  outline: none;
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="footer bg-primary-color pb-4">
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-md-4 primary-color">
+                    <p class="secondary-color bg-secondary-color fw-bold fs-4 p-2 rounded">Contato</p>
+                    <p><i class="bi bi-envelope pe-1"></i>fake.email@fakeemail.fak</p>
+                    <p><i class="bi bi-telephone pe-1"></i>(00) 99999-0000</p>
+                    <hr>
+                </div>
+                <div class="col-md-4 primary-color">
+                    <p class="secondary-color bg-secondary-color fw-bold fs-4 b-2 p-2 rounded">Informações</p>
+                    <p>Endereço: Avenida José Maria 4, 432, Rua A1, Tundio - SP</p>
+                    <p>CNPJ: 00.111.222/3333-44</p>
+                    <hr>
+                </div>
+                <div class="col-md-4 primary-color">
+                    <p class="secondary-color bg-secondary-color fw-bold fs-4 b-2 p-2 rounded">Direitos Autorais</p>
+                    <p>&copy; 2024 Hiago Laureano</p>
+                    <p>Imagens projetadas e fornecidas por Freepik</p>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Footer"
+}
+
+</script>
+
+<style scoped>
+.footer{
+    width: 100%;
+    height: auto;
+}
+</style>
