a0733b3c8cfeba19ea7e2c062477d7b8ebcba772
Hiago Laureano <hiagolaureano@hotmail.com>
2024-09-11 10:49:10 -0300

    create Alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="alert-main">
+        <div v-if="type === 'info'" class="alert alert-info d-flex align-items-center" role="alert"><i class="bi bi-info-square-fill me-2"></i> {{text}}</div>
+        <div v-else-if="type === 'danger'" class="alert alert-danger d-flex align-items-center" role="alert"><i class="bi bi-exclamation-triangle-fill me-2"></i> {{text}}</div>
+        <div v-else-if="type === 'warning'" class="alert alert-warning d-flex align-items-center" role="alert"><i class="bi bi-exclamation-triangle-fill me-2"></i> {{text}}</div>
+        <div v-else class="alert alert-success d-flex align-items-center" role="alert"><i class="bi bi-check-circle-fill me-2"></i> {{text}}</div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Alert",
+    props: {
+        type: {
+            type: String,
+            required: false,
+            default: "success"
+        },
+        text: {
+            type: String,
+            required: false,
+            default: ""
+        },
+    }
+}
+</script>
